a58d98a38d7afffc10278a9e52d2cb06a2da67ae
Andrei <andreii.lapschow1999@mail.ru>
2020-10-21 17:29:46 +0400

    deleting unnecessary characters. word count.

--- main.py
+++ main.py
@@ -3,10 +3,33 @@
 # подсчитывающую количество слов в нём
 # и сохраняющую в выходной файл N (задаётся пользователем) наиболее часто встречаемых слов.
 
+
+
+
 def readFile(Filename):
     with open(Filename,"r", encoding="utf8") as myfile:
         strings = myfile.readlines()
-    return strings
+    s=deletePunctuationMarks(strings)
+    return s
+
+
+
+def deletePunctuationMarks(str):
+    for i in range(len(str)):
+        str[i] = str[i].replace(':','')
+        str[i] = str[i].replace(',', '')
+        str[i] = str[i].replace('.', '')
+        str[i] = str[i].replace('!', '')
+        str[i] = str[i].replace('?', '')
+        str[i] = str[i].replace('&', '')
+        str[i] = str[i].replace('"', '')
+        str[i] = str[i].replace(';', '')
+        str[i] = str[i].replace('(', '')
+        str[i] = str[i].replace(')', '')
+        str[i]=str[i].replace('-', ' ')
+        str[i] = str[i].replace('—', ' ')
+    return str
+
 
 def splitStrings(strings):
     split_string = []
@@ -19,5 +42,14 @@ def wordsFromDocument(filename):
     spl_str=splitStrings(str)
     return spl_str
 
+def countWord(str):
+    count=0
+    for s in str:
+        count+=len(s)
+    return count
+
+
+
 if __name__ == '__main__':
-    print(wordsFromDocument("text.txt"))
+
+    str=wordsFromDocument("text.txt")
