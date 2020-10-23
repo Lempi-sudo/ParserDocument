db9204d9f47e5335450b6f0a6931668e7cc8a022
Andrei <andreii.lapschow1999@mail.ru>
2020-10-23 23:17:52 +0400

    verison 1.0

--- main.py
+++ main.py
@@ -6,15 +6,21 @@
 
 
 
-def readFile(Filename):
-    with open(Filename,"r", encoding="utf8") as myfile:
+
+def listWordsFromDocument(filename):
+    string = readFile(filename)
+    spl_str = splitStrings(string)
+    worlds = listworld(spl_str)
+    return worlds
+
+
+def readFile( filename ):
+    with open(filename,"r", encoding="utf8") as myfile:
         strings = myfile.readlines()
-    s=deletePunctuationMarks(strings)
-    return s
+    stringWithoutPunctuationSumbol=deletePunctuationSumbol(strings)
+    return stringWithoutPunctuationSumbol
 
-
-
-def deletePunctuationMarks(str):
+def deletePunctuationSumbol(str):
     for i in range(len(str)):
         str[i] = str[i].replace(':','')
         str[i] = str[i].replace(',', '')
@@ -30,26 +36,57 @@ def deletePunctuationMarks(str):
         str[i] = str[i].replace('—', ' ')
     return str
 
-
 def splitStrings(strings):
     split_string = []
     for st in strings:
         split_string.append(st.split())
     return split_string
 
-def wordsFromDocument(filename):
-    str=readFile(filename)
-    spl_str=splitStrings(str)
-    return spl_str
+def numbercWordinFile(filename):
+    listworld = listWordsFromDocument(filename)
+    number = len(listworld)
+    return number
 
-def countWord(str):
-    count=0
-    for s in str:
-        count+=len(s)
-    return count
+
+def dictwordRepetitionСounting(worldlist):
+    repitworld = {}
+    for world in worldlist:
+        if world not in repitworld:
+            repitworld[world] = int(1)
+        else:
+            repitworld[world] += 1
+    return repitworld
+
+#возвращает список слов
+def listworld(text):
+    listWord = []
+    for string in text:
+        for world in string:
+            listWord.append(world)
+    return listWord
+
+def WriteFile( amoutworld , listrepitworld , filename ):
+    with open(filename,'w', encoding="utf8") as file:
+        file.write('количество слов в файле='+str(amoutworld)+'\n')
+        file.write('Самые часто встречающиеся слова в файле\n')
+        sort_list=sorted(listrepitworld.items(), key=lambda k:k[1] , reverse=True)
+        file.write('слово\t количество слов\t\n')
+        for para in sort_list:
+            file.write(para[0]+'=' +str(para[1])+'\n')
+
+
+
 
 
 
 if __name__ == '__main__':
 
-    str=wordsFromDocument("text.txt")
+    filename=input('enter file name:')
+
+    dict=dictwordRepetitionСounting(listWordsFromDocument(filename))
+
+    WriteFile(numbercWordinFile(filename),dict,'res.txt')
+
+
+
+
