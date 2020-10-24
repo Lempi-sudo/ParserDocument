36505dd2176b306ba023b07230be8f854873f551
Andrei <andreii.lapschow1999@mail.ru>
2020-10-24 21:15:02 +0400

    email parse finish!

--- emailParser.py
+++ emailParser.py
@@ -1,20 +1,36 @@
+#Создать текстовый документ содержащий список адресов электронных почт,
+# их можно придумать самому. Количество не менее 50.
+#Записать в выходной файл список доменов электронных почт из входного файла.
 
-def ReadFile(filename):
+
+import re
+
+def ReadFile2(filename):
     with open(filename,"r", encoding="utf8") as myfile:
-        string=myfile.readlines()
-    return string
+        text = myfile.read()
+    return text
+
+def WriteFile(listemail , filename ):
+    with open(filename,'w', encoding="utf8") as file:
+        file.write('домены электронной почты : \n')
+        n=0
+        for email in listemail:
+            file.write(email+' ; ')
+            n+=1
+            if n%5==0:
+                file.write('\n')
 
 
-def textcreate(liststring):
-    t = ''
-    for string in liststring:
-        t += string
-    return t
+def ParseEmail(text):
+    textlookfor = r"[@][\w-]+\.+[\w.]+"
+    allresulr = re.findall(textlookfor, text)
+    return allresulr
 
 
 if __name__ == '__main__':
-    text=ReadFile('email.txt')
+    text=ReadFile2('email.txt')
+    listemail=ParseEmail(text)
+    WriteFile(listemail,'emailparse.txt')
 
-    textlookfor=r"[п]\w{6}\s"
 
 
